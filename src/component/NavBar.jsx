f6c416b7c4f8d163bd885d5a2ea4cf145c8c1b7c
Ali0496 <turdiboev0496@gmail.com>
2025-07-18 11:27:46 +0500

    navbar has been updated. responsive design and modal added

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -1,13 +1,41 @@
 import logo from "../image/Headline.svg";
 import "../style/navbar.css";
+import { useRef } from "react";
 
 function NavBar() {
+  const modalRef = useRef();
+  const cardModalRef = useRef();
+
+  const openModal = () => {
+    modalRef.current.classList.add("show__modal");
+  };
+
+  const closeModal = () => {
+    modalRef.current.classList.remove("show__modal");
+  };
+
+  const openCardModal = () => {
+    cardModalRef.current.classList.add("show__card__modal");
+  };
+
+  const removeCardMOdal = () => {
+    cardModalRef.current.classList.remove("show__card__modal");
+  };
   return (
     <div className="navbar">
       <h4>Free Shipping for all orders over $100</h4>
       <hr />
 
       <nav>
+        <li onClick={openModal} className="menu">
+          Menu
+        </li>
+        <div className="menu__modal" ref={modalRef}>
+          <li>Home</li>
+          <li>Collection</li>
+          <li>About</li>
+          <span onClick={closeModal}>x</span>
+        </div>
         <ul className="main__lists">
           <li>Home</li>
           <li>Collection</li>
@@ -19,6 +47,15 @@ function NavBar() {
           <li>Search</li>
           <li>Card(0)</li>
         </ul>
+        <div ref={cardModalRef} className="card__modal">
+          <li>Account</li>
+          <li>Search</li>
+          <li>Card(0)</li>
+          <span onClick={removeCardMOdal}>x</span>
+        </div>
+        <li className="card" onClick={openCardModal}>
+          Card(0)
+        </li>
       </nav>
     </div>
   );
